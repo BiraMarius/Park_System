f51d21e32f202cd78dc2fd88f832c4d8d5671042
MariusB <92020282+BiraMarius@users.noreply.github.com>
2024-03-11 21:55:50 +0200

    Serializare rapoarte 50% done

--- src/main/java/org/example/park/report/ReportOperations.java
+++ src/main/java/org/example/park/report/ReportOperations.java
@@ -2,14 +2,15 @@ package org.example.park.report;
 
 import org.example.park.vehicle.Car;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.example.park.operations.util.Constants.FILE_NAME;
+import static org.example.park.operations.util.Constants.ROOT_PATH;
 
 public class ReportOperations implements IReportOperations {
     public static Report searchReportByDate(List<Report> reports, String date){
@@ -47,6 +48,41 @@ public class ReportOperations implements IReportOperations {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        public static void serializare(List <Car> cars){
+//            closeSerialization(cars, ROOT_PATH + "\\" + FILE_NAME);
+//        }
+//
+//        public static List<Car> deserializare(){
+//            return initDeserialization( ROOT_PATH + "\\" + FILE_NAME);
+//            //cleanFile();
+//        }
+//
+//
+//        public static List<Car> initDeserialization( String fileName) {
+//            try(FileInputStream inputStream = new FileInputStream(fileName);
+//                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream );) {
+//                return  (ArrayList) objectInputStream.readObject();
+//            }catch (FileNotFoundException e) {
+//                System.out.println("File not found: " + fileName);
+//                // Handle this exception as needed, e.g., log or inform the user
+//            } catch (IOException | ClassNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//
+//
+//        private static void closeSerialization(List<Car> cars, String filename){
+//            try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream((filename)))){
+//                out.writeObject(cars);
+//                System.out.println("Objects written to file successfully.");
+//            } catch (IOException e){
+//                e.printStackTrace();
+//            }
+//        }
+
+
     }
 
     public static void addCarsToCt(Report report){
